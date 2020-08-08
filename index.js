d02e401c7c1732a698a11325beaff82abb8b2f2f
Vũ Khương Duy <vukhuongduy23@gmail.com>
2020-08-08 17:03:25 +0700

    add procfile

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ app.use(express.static(__dirname + "public"));
 app.use("/a", express.static(__dirname + "/public"));
 app.use("/", express.static(__dirname + "/public/index.html"));
 
-var server = app.listen(3001, function () {
+const port = process.env.PORT || 3000;
+var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log(`Server connected on host ${host} and port ${port}`);
